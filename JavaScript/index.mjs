5693d74a3481f5c081b173024d6db2e5f8a4d54b
eny852 <enyman825@gmail.com>
2022-10-11 08:31:36 +0200

    zobrazovanie ludi

--- JavaScript/index.mjs
+++ JavaScript/index.mjs
@@ -1,24 +1,11 @@
 import {Clovek} from './clovek.mjs';
 import {Telo} from "./telo.mjs";
-import {userInput} from "./helpers.mjs";
-
-function createNewClovek(paName, paHight, paAge, paWeight, paTelo){
-    return {
-        name : paName,
-        height : paHight,
-        age : paAge,
-        weight : paWeight,
-        telo : paTelo,
-        printClovek : function (){
-            return `${this.name} ${this.height} ${this.age} ${this.weight} ${this.telo.printTelo()}`;
-        }
-    }
-}
+//import {userInput} from "./helpers.mjs";
 
 const t1 = new Telo(1,2,2);
 const t2 = new Telo(2,1,1);
 
-const jurko = createNewClovek("Jurko",180,25,80,t2);
+const jurko = new Clovek("Jurko",180,25,80,t2);
 
 const sona = new Clovek("Sona",177,20,60,t1);
 
@@ -26,6 +13,9 @@ let arr = [sona,jurko];
 
 for(const i in arr){
     console.log(arr[i].printClovek());
+    const li = document.createElement("li");
+    li.textContent = arr[i].printClovek();
+    document.getElementById("ol1").appendChild(li);
 }
 
-console.log(userInput());
+//console.log(userInput());
